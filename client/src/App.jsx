fc98f76fa954253e95892b1ad74b9451055915e4
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-03-05 19:09:52 +0200

    chore: set up main routes structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from "./pages/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "register", element: <Register /> },
+      { path: "dashboard", element: <DashboardLayout /> },
+      { path: "login", element: <Login /> },
+    ],
+  },
+]);
 
 const App = () => {
-  return <h1>App Hello</h1>;
+  return <RouterProvider router={router} />;
+  // return <h1>Hello</h1>;
 };
 
 export default App;
