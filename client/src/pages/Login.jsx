12f5926d9f88b8d274140b7ba1777f34d70b3c85
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-03-06 19:24:51 +0200

    feat(front/ui) add register page and login page design

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { FormRow, Logo } from "../components";
+import styled from "styled-components";
+
 const Login = () => {
   return (
-    <>
-      <div>Login</div>
-      <Link />
-    </>
+    <Wrapper>
+      <form action="" className="form">
+        <Logo />
+        <h4>Login</h4>
+
+        <FormRow type="email" name="email" defaultValue="o@gm.com" />
+
+        <FormRow type="password" name="password" defaultValue="Secret!1" />
+
+        <button type="submit" className="btn btn-block">
+          Login
+        </button>
+
+        <button type="button" className="btn btn-block">
+          Explore the app
+        </button>
+
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h4 {
+    text-align: center;
+    margin-bottom: 1.38rem;
+  }
+
+  p {
+    margin-top: 1rem;
+    text-align: center;
+    line-height: 1.5;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letter-spacing);
+    margin-left: 0.25rem;
+  }
+`;
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,88 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import { FormRow, Logo } from "../components";
+import styled from "styled-components";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Wrapper>
+      <form action="" className="form">
+        <Logo />
+        <h4>Register</h4>
+
+        <FormRow
+          type={"text"}
+          name={"firstName"}
+          labelText={"first name"}
+          defaultValue={"Omar"}
+        />
+
+        <FormRow
+          type={"text"}
+          name={"lastName"}
+          labelText={"last name"}
+          defaultValue={"Sanad"}
+        />
+
+        <FormRow type={"text"} name={"location"} defaultValue={"Port-Said"} />
+
+        <FormRow type={"email"} name={"email"} defaultValue={"o@g.com"} />
+
+        <FormRow type="password" name="password" defaultValue="Serect!1" />
+
+        <button type="submit" className="btn btn-block">
+          Register
+        </button>
+
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h4 {
+    text-align: center;
+    margin-bottom: 1.38rem;
+  }
+
+  p {
+    margin-top: 1rem;
+    text-align: center;
+    line-height: 1.5;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letter-spacing);
+    margin-left: 0.25rem;
+  }
+`;
 
 export default Register;
