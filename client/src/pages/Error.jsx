d272dff9dc5b8386c1f6cbc8d8e46192de1d6d9b
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-03-06 15:30:32 +0200

    feat: add error page
    - Display error message
    - Basic styling applied

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,17 +1,64 @@
 import React from "react";
 
+import styled from "styled-components";
 import { Link, useRouteError } from "react-router-dom";
 
+import img from "../assets/images/not-found.svg";
+
 const Error = () => {
-  const error=  useRouteError();
-  console.log(error);
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh! page not found</h3>
+          <p>We can seem to find the page you're looking for</p>
+          <Link to="/dashboard">Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
 
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to="/">Back To Home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 90vw;
+    max-width: 600px;
+    margin-bottom: 2rem;
+    margin-top: -3rem;
+  }
+
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    line-height: 1.5;
+    margin-top: 0.5rem;
+    margin-bottom: 1rem;
+    color: var(--text-secondary-color);
+  }
+
+  a {
+    color: var(--primary-500);
+    text-transform: capitalize;
+  }
+`;
 
 export default Error;
