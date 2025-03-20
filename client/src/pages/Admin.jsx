a315a1b0bf24aa6d08ca29b81eb451964a67be1c
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-03-20 16:20:08 +0200

    feat(frontend/Admin Page): admin page done

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,7 +1,10 @@
 import { toast } from "react-toastify";
 import { redirect, useLoaderData } from "react-router-dom";
+import styled from "styled-components";
 
 import customFetch from "../utils/customFetch";
+import { StatsItem } from "../components";
+import { FaCalendarCheck, FaSuitcaseRolling } from "react-icons/fa";
 
 export const loader = async () => {
   try {
@@ -18,11 +21,38 @@ const Admin = () => {
   const { jobs, users } = useLoaderData();
   console.log(jobs, users);
 
-  return <div>
-    <h1>Admin</h1>
-    <h3>{jobs}</h3>
-    <h3>{users }</h3>
-  </div>;
+  return (
+    <Wrapper>
+      <StatsItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatsItem
+        title="total jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<FaCalendarCheck />}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  display: grid;
+  row-gap: 2rem;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+  }
+
+  @media (min-width: 1120px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`;
 
 export default Admin;
